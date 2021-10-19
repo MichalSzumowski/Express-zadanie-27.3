d9e032d5f62885b5b3761a7dfccf20f48380ff4e
Michał Szumowski <mic.szumowski@gmail.com>
2021-10-19 19:12:44 +0200

    Add contact subpage with functionality - ex 27.4

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer  = require('multer');
+const upload = multer();
 
 const app = express();
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.post('/contact/send-message', upload.single('image'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+  const image = req.file;
+
+  if(author && sender && title && message && image) {
+    res.render('contact', { isSent: true, name: image.originalname });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+});
 
 app.get('/', (req, res) => {
   res.render('index');
